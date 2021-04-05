5b83c7031d36bdafd5eb9c591c60de47550c7935
naravich <naravich@enres.co>
2021-04-05 19:05:27 +0700

    first commit

---
+++ src/01-word-count.py
@@ -0,0 +1,12 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf=conf)
+
+lines = sc.textFile("file:///opt/bitnami/spark/datasets/book_full.txt")
+wordCounts = lines.countByValue()
+
+for word, count in wordCounts.items():
+    cleanWord = word.encode('ascii', 'ignore')
+    if (cleanWord):
+        print(cleanWord.decode() + " " + str(count))
