e5f49e1f8b991c48b8f00cb37e2077ae6bd3df0c
naravich <naravich@enres.co>
2021-04-06 13:51:29 +0700

    spark-ws

--- src/01-word-count.py
+++ src/01-word-count.py
@@ -3,6 +3,6 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().setMaster("local").setAppName("WordCount")
 sc = SparkContext(conf=conf)
 
-lines = sc.textFile("file:///opt/bitnami/spark/datasets/book_full.txt")
+lines = sc.textFile("file:///opt/bitnami/spark/datasets/book.txt")
 for word in lines.collect():
     print(word.encode('ascii', 'ignore'))
